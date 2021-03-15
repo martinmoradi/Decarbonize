d256197139d4a9101eea11f68d8ea913ddf0bfe2
Martin Moradi <moradi.martin@gmail.com>
2021-03-15 15:55:40 +0100

    setup load assets + fonts

--- App.tsx
+++ App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
 import { AppRoutesParamsList } from "./src/components/Navigation";
 import {AuthenticationNavigator} from './src/Authentication'
 import {HomeNavigator} from './src/Home';
-
+import * as React from 'react';
+import 'react-native-gesture-handler';
+import LoadAssets from './src/components/LoadAssets';
 
 const AppStack = createStackNavigator<AppRoutesParamsList>();
 
-export default function App() {
+const fonts = {
+  'Avenir-Bold': require('./assets/fonts/AvenirNextLTPro-Bold.otf'),
+  'Avenir-Semibold': require('./assets/fonts/AvenirNextLTPro-Demi.otf'),
+  'Avenir-Regular': require('./assets/fonts/AvenirNextLTPro-Medium.otf'),
+};
+
+
+const App = () => {
   return (
+      <LoadAssets {...{ fonts }}>
     <NavigationContainer>
       <AppStack.Navigator headerMode="none">
         <AppStack.Screen  name="Authentication" component={AuthenticationNavigator}/>
         <AppStack.Screen  name="Home" component={HomeNavigator}/>
       </AppStack.Navigator>
     </NavigationContainer>
-
+    </LoadAssets>
   );
-}
+};
 
 
+export default App;
