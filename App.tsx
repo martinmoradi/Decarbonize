74db24dc65681881f32cd01f0205a54b2d6a51c0
Uriel <uriel.sautron@gmail.com>
2021-03-17 19:43:21 +0100

    Create GlobalProvider

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
 import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AuthenticationNavigator } from './src/Authentication';
 import LoadAssets from './src/components/LoadAssets';
+import { AppRoutesParamsList } from './src/components/Navigation';
 import { ThemeProvider } from './src/components/Theme';
+import { HomeNavigator } from './src/Home';
+
+const AppStack = createStackNavigator<AppRoutesParamsList>();
 
 const fonts = {
   'Avenir-Bold': require('./assets/fonts/AvenirNextLTPro-Bold.otf'),
