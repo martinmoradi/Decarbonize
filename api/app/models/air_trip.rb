e567857f347bf3143dd3a5cb503415f0b9dd9b39
Martin Moradi <moradi.martin@gmail.com>
2021-03-19 21:39:47 +0100

    Revert "hotfix: monthly emitted carbon model fixed emissions"
    This reverts commit 8f60ba2994a48927eb077792c20679cf55582ba8.

--- api/app/models/air_trip.rb
+++ api/app/models/air_trip.rb
@@ -1,3 +1,7 @@
+# Schema.rb
+#  t.boolean "round_trip"
+#  t.integer "distance"
+
 class AirTrip < ApplicationRecord
   has_one :emission, as: :emissionable, dependent: :destroy
   has_many :airports
