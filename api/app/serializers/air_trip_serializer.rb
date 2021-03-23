f90e58e0308856e3eef6f7380f3bda0430bd26de
Martin-Louvard <martin.louvard@gmail.com>
2021-03-23 15:19:27 +0100

    fixed db bug

--- api/app/serializers/air_trip_serializer.rb
+++ api/app/serializers/air_trip_serializer.rb
@@ -1,6 +1,5 @@
 class AirTripSerializer
   include JSONAPI::Serializer
-  attributes :round_trip, :departure_latitude, :departure, :arrival, :departure_longitude,  :arrival_latitude, :arrival_longitude
 
   attribute :emmited_carbon do |object|
     object.air_trip.emmited_carbon
@@ -8,6 +7,18 @@ class AirTripSerializer
 
   attribute :created_at do |object|
     object.air_trip.created_at
+  end
+
+  attribute :departure do |object|
+    object.air_trip.departure
+  end
+
+  attribute :arrival do |object|
+    object.air_trip.arrival
+  end
+
+  attribute :round_trip do |object|
+    object.air_trip.round_trip
   end
 
 end
