8b3f39aa6a70e3f801d290b639c741a0ead4dbec
Martin-Louvard <martin.louvard@gmail.com>
2021-03-24 10:55:45 +0100

    deleted state folder,replaced by redux folder

--- src/state/types/index.ts
+++
@@ -1,3 +0,0 @@
-export { AuthActionType, UserParamsType, UserType } from './authTypes';
-export { EmissionsActionType, EmissionsType } from './emissionsTypes';
-export { TripActionType } from './tripTypes';
